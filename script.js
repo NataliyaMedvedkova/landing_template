e093c9e90a9c5792f2e371e1e41b645b7fb6b34d
Nataliya Medvedkova <kuznya.nat@gmail.com>
2018-05-29 23:18:12 +0500

    fixed indentation

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 menu.onclick = function myFunction() {
-    var x = document.getElementById('myTopnav');
-
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById('myTopnav');
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
